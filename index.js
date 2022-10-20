6ddd2345d91bbe350306b239ee288524eef51ba4
Victoria Beasley <iesnoth91@gmail.com>
2022-10-20 18:51:24 -0500

    index not working

--- index.js
+++ index.js
@@ -3,15 +3,34 @@ const express = require('express')
 const app = express()
 
 //Middleware
-app.set(`view engine`,`jsx`)
+app.set(`view engine`, `jsx`)
 app.engine(`jsx`, require(`express-react-views`).createEngine())
 
-app.use(`/places`,require(`./controllers/places`))
-
-app.get(`/`,(req,res) => {
+app.use(`/places`, require(`./controllers/places`))
+//GET homepage
+app.get(`/`, (req, res) => {
     res.render(`home`)
 })
-app.get(`*`,(req,res) => {
+//GET /places
+app.get(`/`, (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+    }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+    }]
+
+    res.render(`places/index`, { places })
+})
+
+app.get(`*`, (req, res) => {
     res.status(404).render(`error404`)
 })
 
