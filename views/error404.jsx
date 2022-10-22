08b25876092368c961852c29fa33691963405746
Victoria Beasley <iesnoth91@gmail.com>
2022-10-22 10:53:11 -0500

    404 img

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,6 +6,12 @@ function error404 () {
         <Def>
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
+                <div>
+                    <img src="/images/sad-pizza.gif" alt="man wipes tears with whole pizza"/>
+                <div>
+                    Photo by <a href="https://imgur.com/user/queeferito">A Scandlous Burrito</a> on <a href="https://imgur.com/gallery/gYv6Q">imgur</a>
+                </div>
+                </div>
                 <p>Oops, sorry, we can't find this page!</p>
             </main>
         </Def>
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,13 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/curry.jpg" alt="indian curry with naan"/>
+                <div>
+                    Photo by <a href="https://unsplash.com/@mekalluakella">Kalyani Akella</a> on <a href="https://unsplash.com/photos/gml9g1kRQcM">Unsplash</a>
+                </div>
+                </div>
+
                 <a href="/places">
                     <button className='btn-primary'>Places Page</button>
                 </a>
