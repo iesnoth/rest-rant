9333fd4a809d60555d436cc389602e358db21bf8
Victoria Beasley <iesnoth91@gmail.com>
2022-10-22 12:37:01 -0500

    footer

--- views/default.jsx
+++ views/default.jsx
@@ -2,7 +2,7 @@ const React = require(`react`)
 
 function Def (html) {
 return (
-    <html>
+    <html id="default">
         <head>
             <title>Title</title>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossOrigin="anonymous"></link>
@@ -11,6 +11,7 @@ return (
                 {html.children}
             </body>
         </head>
+        <footer id="footer"> Instagram - LinkedIN - Email - Smoke Signals - Morse Code</footer>
     </html>
 )
 }
