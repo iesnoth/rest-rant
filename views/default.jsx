4c0088aacbdf76d677407fd462c49d5270fc2936
Victoria Beasley <iesnoth91@gmail.com>
2022-10-24 18:12:25 -0500

    navbar and style

--- views/default.jsx
+++ views/default.jsx
@@ -1,19 +1,32 @@
 const React = require(`react`)
 
-function Def (html) {
-return (
-    <html id="default">
-        <head>
-            <title>Title</title>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossOrigin="anonymous"></link>
-            <link rel="stylesheet" href="/css/style.css"/>
-            <body>
-                {html.children}
-            </body>
-        </head>
-        <footer id="footer"> Instagram - LinkedIN - Email - Smoke Signals - Morse Code</footer>
-    </html>
-)
+function Def(html) {
+    return (
+        <html id="default">
+            <head>
+                <title>Title</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossOrigin="anonymous"></link>
+                <link rel="stylesheet" href="/css/style.css" />
+                <body>
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href="/">Home</a>
+                            </li>
+                            <li>
+                                <a href="/places">Restaurants</a>
+                            </li>
+                            <li>
+                                <a href="/places/new">Add a Restraunt</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    {html.children}
+                </body>
+            </head>
+            <footer id="footer"> Instagram - LinkedIN - Email - Smoke Signals - Morse Code</footer>
+        </html>
+    )
 }
 
 module.exports = Def
