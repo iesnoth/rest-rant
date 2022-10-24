19de356d55e10194807b59a45275e05e0cbb751a
Victoria Beasley <iesnoth91@gmail.com>
2022-10-24 18:24:44 -0500

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -7,15 +7,15 @@ router.get('/new', (req, res) => {
 })
 
 //POST
-router.post(`/`,(req,res) =>{
+router.post(`/`, (req, res) => {
   console.log(req.body)
-  if(!req.body.pic) {
-    req.body.pic= 'http://placekitten.com/400/400'
+  if (!req.body.pic) {
+    req.body.pic = 'http://placekitten.com/400/400'
   }
-  if(!req.body.city){
+  if (!req.body.city) {
     req.body.city = "Anytown"
   }
-  if(!req.body.state){
+  if (!req.body.state) {
     req.body.state = "USA"
   }
   places.push(req.body)
@@ -23,8 +23,22 @@ router.post(`/`,(req,res) =>{
 })
 
 //GET /places
-router.get(`/`, (req, res) => { 
+router.get(`/`, (req, res) => {
   res.render(`places/index`, { places })
+})
+
+//SHOW
+router.get(`/:id`, (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render(`error404`)
+  }
+  else if (!places[id]){
+    res.render(`error404`)
+  }
+  else {
+    res.render(`places/show`,{place: places[id]})
+  }
 })
 
 module.exports = router
